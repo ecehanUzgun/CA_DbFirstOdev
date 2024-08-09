ec78219980569cddc2307b904636f501d03ffb1d
ecehanUzgun <ecehan766@gmail.com>
2024-08-09 11:20:20 +0300

    Linq to Entity, GroupBy() kullanımı

--- CA_NorthwindDbFirst/Concretes/Services/OrderService.cs
+++ CA_NorthwindDbFirst/Concretes/Services/OrderService.cs
@@ -16,22 +16,41 @@ namespace CA_NorthwindDbFirst.Concretes.Services
         {
             //select EmployeeID,CustomerID,COUNT(*) from Orders group by EmployeeID,CustomerID
             #region Linq to SQL 
-            var format = from o in _db.Orders
-                         group o by new { o.EmployeeId, o.CustomerId }
-                             into g
-                         select new
-                         {
-                             Employee = g.Key.EmployeeId,
-                             Customer = g.Key.CustomerId,
-                             Count = g.Count()
-                         };
+            //var format = from o in _db.Orders
+            //             group o by new { o.EmployeeId, o.CustomerId }
+            //                 into g
+            //             select new
+            //             {
+            //                 Employee = g.Key.EmployeeId,
+            //                 Customer = g.Key.CustomerId,
+            //                 Count = g.Count()
+            //             };
 
-            foreach (var item in format.ToList())
+            //foreach (var item in format.ToList())
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine(format.ToList().Count()); 
+            #endregion
+
+            #region Linq to Entity
+            var query = _db.Orders.GroupBy(x => new
+            {
+                x.EmployeeId,
+                x.CustomerId
+            }).Select(x => new
+            {
+                Employee = x.Key.EmployeeId,
+                Customer = x.Key.CustomerId,
+                Count = x.Count()
+            });
+            foreach (var item in query.ToList())
             {
                 Console.WriteLine(item);
             }
-            Console.WriteLine(format.ToList().Count()); 
+            Console.WriteLine(query.ToList().Count()); 
             #endregion
+
         }
 
         public List<Order> GetOrdersByDate(DateTime startDate, DateTime endDate)
